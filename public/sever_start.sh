9e6396c8809df5d19bb6f48172e724e9df711946
Bastian Hodapp <bastianhodapp@gmail.com>
2022-11-27 13:45:45 +0100

    improve server start bash

--- public/sever_start.sh
+++ public/sever_start.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+
+# Add nvm to port
 export PATH=/home/pi/.config/nvm/versions/node/v16.14.0/bin:$PATH
 echo $PATH
-serve -s -l 12345 &
-chromium-browser http://localhost:12345
+
+# Kill all old processes using Port 12345
+kill $(lsof -t -i:12345)
+
+#Start server
+serve ./ -s -l 12345 &
+chromium-browser --start-fullscreen http://localhost:12345
