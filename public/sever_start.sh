aa9e286f89594b6835903b84d0b6bbd3e1e681bf
Bastian Hodapp <bastianhodapp@gmail.com>
2023-06-13 11:00:01 +0200

    server start code

--- public/sever_start.sh
+++ public/sever_start.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-# Add nvm to port
-export PATH=/home/pi/.config/nvm/versions/node/v16.14.0/bin:$PATH
-echo $PATH
-
-# Kill all old processes using Port 12345
-kill $(lsof -t -i:12345)
-
-#Start server
-serve ./ -s -l 12345 &
-chromium-browser --start-fullscreen http://localhost:12345
+cd ../
+yarn preview --port 3000 &
+sleep 3
+chromium-browser --start-fullscreen http://localhost:3000
+sudo nano /etc/rc.local
